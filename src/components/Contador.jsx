40d8519d8c825b07c41871dd51d542aca772976c
K Stephan Torres <kevinstephantorres@gmail.com>
2023-12-11 11:32:48 -0300

    Se agrego parte de logica al componenete Contador

--- src/components/Contador.jsx
+++ src/components/Contador.jsx
@@ -1,12 +1,12 @@
 
 const Contador = () => {
-    
+    const numero= 0
     
     return (
         <article className="text-center">
             <h2>Contador</h2>
-            <h3>10</h3>
-            <button className="btn btn-primary">+</button>
+            <h3>{numero}</h3>
+            <button className="btn btn-success" onClick={(numero) => numero++}>+</button>
         </article>
     );
 };
